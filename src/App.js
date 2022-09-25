83033f72f285075c004f9e8ae915329d4760e057
nll004 <loninath17@gmail.com>
2022-09-25 07:32:20 -0400

    styling

--- src/App.js
+++ src/App.js
@@ -9,15 +9,14 @@ function App() {
     <div className="App">
       <div className="container">
         <h1>Lights Out</h1>
-        <p>Select a cell to toggle lights</p>
-        <p>Wasting electricity is not great! Turn all of the lights out to win!</p>
-      </div>
-      <div className="Board">
-        <table className="Board-table">
-          <tbody>
-            <Board nrows={6} ncols={6} chanceLightStartsOn={.1}/>
-          </tbody>
-        </table>
+        <p>Select a cell to toggle lights. Turn all of the lights off to win!</p>
+        <div className="Board">
+          <table className="Board-table">
+            <tbody>
+              <Board nrows={4} ncols={6} chanceLightStartsOn={.5}/>
+            </tbody>
+          </table>
+        </div>
       </div>
     </div>
   );
