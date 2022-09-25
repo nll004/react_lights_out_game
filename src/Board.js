2910bc969e297f0c3b7d3c36f1c8ba6adfd9e94f
nll004 <loninath17@gmail.com>
2022-09-24 21:37:00 -0400

    click event working as intended, needs win logic added

--- src/Board.js
+++ src/Board.js
@@ -37,64 +37,65 @@ function Board({ nrows, ncols, chanceLightStartsOn }) {
     for(let row = 0; row < nrows; row ++){
       let column = [];
       for(let col = 0; col < ncols; col++){
-        // randomly assign '.' or '0' to cells
-        if(Math.random() > 0.5) column.push(true);
+        // randomly assign true or false to cells
+        if(Math.random() >= chanceLightStartsOn) column.push(true);
         else column.push(false);
       };
       initialBoard.push(column)
     }
-    console.log('Initial board', initialBoard);
     return initialBoard;
   }
 
   function hasWon() {
-    console.log('hasWon func checked')
-
-    if(board.forEach(r => r.every(val => val === false))) {
-      console.log('Winner!')
-    }
+    console.log('hasWon func checked', board)
+    return board.map(r=>r.every(val => val === false))
   }
 
   function flipCellsAround(coord) {
+
     setBoard(oldBoard => {
-      const [y, x] = coord.split("-").map(Number);
+      const [x, y] = coord.split("-").map(Number);
 
-      const flipCell = (y, x, boardCopy) => {
+      const flipCell = (x, y, boardCopy) => {
         // if this coord is actually on board, flip it
-
         if (x >= 0 && x < ncols && y >= 0 && y < nrows) {
           boardCopy[y][x] = !boardCopy[y][x];
         }
+        // change values above, below, right and left if they exist on board
+        if (x - 1 >= 0) boardCopy[y][x-1] = !boardCopy[y][x-1];
+        if (x + 1 < ncols) boardCopy[y][x+1] = !boardCopy[y][x+1];
+        if (y - 1 >= 0) boardCopy[y-1][x] = !boardCopy[y-1][x];
+        if (y + 1 < nrows) boardCopy[y+1][x] = !boardCopy[y+1][x];
+
+        return boardCopy
       };
 
-      // TODO: Make a (deep) copy of the oldBoard
+      const boardCopy = JSON.parse(JSON.stringify(board)) // deep copy of board
+      const rearrangedBoard = flipCell(x,y,boardCopy)
 
-      // TODO: in the copy, flip this cell and the cells around it
-
-      // TODO: return the copy
+      return rearrangedBoard
     });
   }
 
   // if the game is won, just show a winning msg & render nothing else
-  if (hasWon()){
-    alert("Horray! All lights are off!")
-    return
-  }
-  else {
-    return (
-      board.map((r,y) =>
-        <tr className="Board-row" key={y}>
-          { r.map((c,x) =>
-              <Cell id={`${x}-${y}`}
-                    isLit={c}
-                    key={`${x}-${y}`}
-              />
-          )}
-        </tr>
-      )
-    )
-  }
+  // if (hasWon){
+  //   return alert("Horray! All lights are off!")
+  // }
 
+  return (
+    board.map((r,y) =>
+      <tr className="Board-row" key={y}>
+        { r.map((c,x) =>
+            <Cell isLit={c}
+                  key={`${x}-${y}`}
+                  id={`${x}-${y}`}
+                  coord={`${x}-${y}`}
+                  flipCellsAround={flipCellsAround}
+            />
+        )}
+      </tr>
+    )
+  )
 }
 
 export default Board;
